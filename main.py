88fbf722aa73fd9f1958c5b96a61387199bfacae
PoddelkaPIX <gnebedyuka@list.ru>
2021-01-01 01:35:53 +0500

    Добавил скачёк, сделал игру во весь экран, начал делать поворот игрока и предупреждение о приближающемся враге

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 
 import pygame
 
+import warnings
+warnings.filterwarnings("ignore")
+
 from sklearn import preprocessing
 
 G_Player_position = [0, 0]
@@ -11,8 +14,8 @@ G_Mouse_position = [0, 0]
 
 pygame.init()
 pygame.display.set_caption('Моя игра')
-size = width, height = 1080, 720
-screen = pygame.display.set_mode(size)
+size = width, height = 1600, 900
+screen = pygame.display.set_mode((size), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
@@ -20,6 +23,7 @@ bullets = pygame.sprite.Group()
 enemy_bullets = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+mobs_warning = pygame.sprite.Group()
 
 fps = 60
 score = 0
@@ -51,11 +55,24 @@ class UI(pygame.sprite.Sprite):
         text = font.render(str(score), True, (100, 255, 100))
         text_x = width - (text.get_height() + 15)
         text_y = 10
-        text_w = text.get_width()
-        text_h = text.get_height()
         screen.blit(text, (text_x, text_y))
-        pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                               text_w + 20, text_h + 20), 1)
+
+
+class Mob_warning(pygame.sprite.Sprite):
+    def __init__(self, enemy_pos):
+        super().__init__(all_sprites)
+        self.image = load_image('arrow.png')
+        self.rect = self.image.get_rect()
+        self.enemy_pos = enemy_pos
+
+    def update(self):
+        if self.enemy_pos[0] > width:
+            self.rect.x = width - 100
+            self.rect.y = self.enemy_pos[1]
+
+        if self.enemy_pos[0] < 0:
+            self.rect.x = width
+            self.rect.y = self.enemy_pos[1]
 
 
 class Button(pygame.sprite.Sprite):
@@ -79,6 +96,8 @@ class Player(pygame.sprite.Sprite):
 
         self.MOVE_SPEED = 3
 
+        self.direction = 'right'
+
         self.width = self.rect.size[0]
         self.height = self.rect.size[1]
 
@@ -88,9 +107,11 @@ class Player(pygame.sprite.Sprite):
         self.move_down = False
 
         self.is_alive = True
+        self.flash = False
 
     def update(self):
         self.move()
+        self.animation_player()
 
     def move(self):
         global G_Player_position
@@ -105,10 +126,44 @@ class Player(pygame.sprite.Sprite):
         elif self.move_down:
             self.rect.y += self.MOVE_SPEED
 
+        if self.flash:
+            self.arr = preprocessing.normalize([[G_Mouse_position[0] - self.rect.x, G_Mouse_position[1] - self.rect.y]])
+            self.rect.x += self.arr[0][0] * 200
+            self.rect.y += self.arr[0][1] * 200
+            self.flash = False
+
+        if self.rect.x > width:
+            self.rect.x -= self.MOVE_SPEED
+        elif self.rect.x < 0:
+            self.rect.x += self.MOVE_SPEED
+        elif self.rect.y > height:
+            self.rect.y -= self.MOVE_SPEED
+        elif self.rect.y < 0:
+            self.rect.y += self.MOVE_SPEED
+
+
     def shoot(self, mouse_pos):
         bullet = Bullet(mouse_pos, self.rect)
         all_sprites.add(bullet)
         bullets.add(bullet)
+
+    def animation_player(self):
+        if G_Player_position[1] - G_Mouse_position[1] == 0:
+            self.direction = 'up'
+        elif G_Player_position[1] - G_Mouse_position[1] < 0 and (G_Player_position[0] - G_Mouse_position[0]) < 200:
+            self.direction = 'down'
+
+        if G_Player_position[0] - G_Mouse_position[0] < 200 and G_Player_position[1] - G_Mouse_position[1] < 300:
+            self.direction = 'right'
+        elif G_Player_position[0] - G_Mouse_position[0] > 200 and G_Player_position[1] - G_Mouse_position[1] > -300:
+            self.direction = 'left'
+
+        if self.direction == 'down':
+            self.image = load_image('player/лицом_1.png')
+        elif self.direction == 'right':
+            self.image = load_image('player/правой_рукой_1.png')
+        elif self.direction == 'left':
+            self.image = load_image('player/левой_рукой_1.png')
 
 
 class Shooter(pygame.sprite.Sprite):
@@ -143,6 +198,7 @@ class Shooter(pygame.sprite.Sprite):
         self.rect.y += self.arr[0][1] * self.SPEED + 0.4
 
         self.hit()
+        self.visible()
 
     def restart(self):
         self.rect.x = width / 2
@@ -158,6 +214,16 @@ class Shooter(pygame.sprite.Sprite):
         hits = pygame.sprite.spritecollide(player, mobs, False)
         if hits:
             self.restart()
+
+    def visible(self):
+        if self.rect.x > width:
+            mobs_warning.add(Mob_warning(self.rect))
+        elif self.rect.x < 0:
+            mobs_warning.add(Mob_warning(self.rect))
+
+        else:
+            for mob in mobs_warning:
+                mob.kill()
 
 
 class Stump(pygame.sprite.Sprite):
@@ -295,9 +361,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEMOTION:
+            G_Mouse_position = event.pos
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if player.is_alive:
                 player.shoot(event.pos)
+                G_Mouse_position = event.pos
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
@@ -308,6 +378,16 @@ while running:
                 player.move_up = True
             elif event.key == pygame.K_s:
                 player.move_down = True
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_1:
+                pygame.display.quit()
+                size = width, height = (1080, 720)
+                screen = pygame.display.set_mode(size)
+            elif event.key == pygame.K_2:
+                pygame.display.quit()
+                size = width, height = 1520, 880
+                screen = pygame.display.set_mode((size), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN)
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 player.move_left = False
@@ -317,6 +397,8 @@ while running:
                 player.move_up = False
             elif event.key == pygame.K_s:
                 player.move_down = False
+            elif event.key == pygame.K_SPACE:
+                player.flash = True
 
     screen.fill((0, 0, 0))
     all_sprites.update()
